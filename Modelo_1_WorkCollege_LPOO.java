b2bd7d0b04764fc5ea5fa52e1359669f7bee5e89
llm1998 <lucas_lmorel@hotmail.com>
2017-03-22 15:30:36 -0300

    Add files via upload

---
+++ Modelo_1_WorkCollege_LPOO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo_1_workcollege_lpoo;
+
+/**
+ *
+ * @author Lucas Morel
+ */
+public class Modelo_1_WorkCollege_LPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Nota_Fiscal.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo_1_workcollege_lpoo;
+
+/**
+ *
+ * @author Lucas Morel
+ */
+public class Nota_Fiscal {
+    
+}
