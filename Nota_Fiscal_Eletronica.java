304ba8cc24d53d743a2d5d48005553fb41cc6fde
Marcelo de Castro <marcelo@castro.eti.br>
2017-03-22 16:13:01 -0300

    Update Nota_Fiscal_Eletronica.java

--- Nota_Fiscal_Eletronica.java
+++ Nota_Fiscal_Eletronica.java
@@ -14,7 +14,11 @@ public class Nota_Fiscal_Eletronica {
 
 	//ESTOU COM DIFICULDADE DE FAZER A HERANÇA QUE O SENHOR PEDIU PARA SER FEITO NO TÓPICO 11 AO 13 NÃO ESTOU CONSEGUINDO RESOLVER.
     
+// extends não é utilizado aqui e sim na definição da classe 
     public .Nota_Fiscal_Eletronica extends Nota_Fiscal {
+	// ^ tem um ponto aqui que não existe
+
+	    // atenção com os blocos 
     public Nota_Fiscal_Eletronica (String _codigo, double _Valor_Total, Cliente _cliente){
             super(_codigo, _Valor_Total, _cliente)
     }
